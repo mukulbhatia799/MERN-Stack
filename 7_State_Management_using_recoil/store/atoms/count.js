31ccb97ba1874a147154d46159361f9865e705cc
Mukul Bhatia <mukulbhatia189@gmail.com>
2024-06-04 21:56:43 +0530

    added example of filteredData selector

--- 7_State_Management_using_recoil/store/atoms/count.js
+++ 7_State_Management_using_recoil/store/atoms/count.js
@@ -8,12 +8,21 @@ export const countAtom = atom({
 // get rid of all useState and useContext from App.jsx.
 
 
-
+// This is a selector.
 export const oddSelector = selector({
     key: "oddSelector",
-    get: (props) => {
-        const count = props.get(countAtom);
+    get: (props) => {                           // this will get the value for the oddSelector.
+        const count = props.get(countAtom);     // As oddSelector depends on countAtom, so when it's value changes, the oddSelector will get re-run and rendered. Selectors can depend on other atoms as well as selectors.
         return count % 2;
     }
 })
-// As oddSelector depends on countAtom, so when it's value changes, the oddSelector will get re-run and rendered. Selectors can depend on other atoms as well as selectors.
+
+
+// export const filteredData = selector({
+//     key: "filteredData",
+//     get: (props) => {
+//         const todos = props.get(todosAtom);
+//         const filter = props.get(filterAtom);
+//         return todos.map(todo => (todo.title.include(filter) && todo.description.include(filter))); 
+//     }
+// })
