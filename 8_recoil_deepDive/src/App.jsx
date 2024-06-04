47391d19cd19f6f94c23adf1ca88f3d019eaefeb
Mukul Bhatia <mukulbhatia189@gmail.com>
2024-06-04 21:57:10 +0530

    just created

---
+++ 8_recoil_deepDive/src/App.jsx
@@ -0,0 +1,8 @@
+
+/*
+Suppose, we have linkedin top bar in a states like networkCount, messagingCount, notificationCount etc. So, one way is to use useState() and another is using recoil state management library.
+*/
+
+export default function App() {
+
+}
